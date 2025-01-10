79d4dcdbcc40dc2da07af1d549b8a5f51449aa38
Florian_Sebulke <florian.sebulke@volkswagen.de>
2025-01-10 16:13:52 +0100

    TEST

---
+++ dummy/src/App.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "sebu-dev-lib";
+
+export function App() {
+  return (
+    <>
+      <div className="text-red-400">JALLLASODOASFO</div>
+      <Layout>
+        <div>HALLLO</div>
+      </Layout>
+    </>
+  );
+}
+
+export default App;

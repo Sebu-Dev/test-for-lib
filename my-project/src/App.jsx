d3ae3492965fa143a7cad0e1c82f3d3f85dfda45
Florian_Sebulke <fsebulke@gmail.com>
2025-01-13 16:12:47 +0100

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { Layout } from "react-component-library";
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;

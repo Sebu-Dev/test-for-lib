98678e8682e96f91dc3f260cbe20628e6c67f058
Florian_Sebulke <fsebulke@gmail.com>
2025-01-14 09:49:44 +0100

    test

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,20 @@
-import { Layout } from "sebu-dev-react-lib";
+import {
+  ButtonCyan,
+  Card,
+  Layout,
+} from "/Users/vwbspk0/Desktop/VsCode/npm-packages/sebu-dev-react-lib";
+
 import "sebu-dev-react-lib/dist/sebu-dev-react-lib.css";
 function App() {
   return (
     <>
       <Layout>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div>
+          <ButtonCyan handleOnClick={() => true} label="TEST THIS BUTTON" />
+          <Card title="rechter Titel" alignTitle="left" alignContent="center">
+            <p>Der Inhalt ist ebenfalls rechts.</p>
+          </Card>
+        </div>
       </Layout>
     </>
   );
